3ca9e683bd6055d48e1bbff8d39038152475d2a4
jimmyli-MXIC <li759847257@gmail.com>
2018-09-20 10:24:39 +0800

    rename push.sh to push.exp

--- push.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/expect
-set timeout 30
-spawn git push origin master
-expect "Username for 'https://github.com':"
-send "jimmyli-mxic\r"
-expect "Password for 'https:jimmyli-mxic@github.com':"
-send "xingyang0502\r"
-interact
